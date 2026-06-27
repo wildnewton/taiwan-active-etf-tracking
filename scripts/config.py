281775741002925936a245b39aaee39b869950c4
wildnewton <75416102+wildnewton@users.noreply.github.com>
2026-06-27 16:33:18 +0800

    Add DB-backed get_etf_config shim

--- scripts/config.py
+++ scripts/config.py
@@ -5,3 +5,9 @@ MONEYDJ_URL_TEMPLATE = (
 
 def get_moneydj_url(code):
     return MONEYDJ_URL_TEMPLATE.format(code=code.upper())
+
+
+def get_etf_config(code):
+    from etf_universe import get_etf_config as _get_etf_config
+
+    return _get_etf_config(code)
