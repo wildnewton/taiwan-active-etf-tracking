42aea45251bd8f728f98537630d4999af228ff58
wildnewton <75416102+wildnewton@users.noreply.github.com>
2026-06-27 17:30:20 +0800

    Remove TRACKED_ETFS compatibility proxy

--- scripts/config.py
+++ scripts/config.py
@@ -11,22 +11,3 @@ def get_etf_config(code):
     from etf_universe import get_etf_config as _get_etf_config
 
     return _get_etf_config(code)
-
-
-class _TrackedEtfs:
-    def _rows(self):
-        from etf_universe import get_active_etfs
-
-        return get_active_etfs()
-
-    def __iter__(self):
-        return iter(self._rows())
-
-    def __len__(self):
-        return len(self._rows())
-
-    def __getitem__(self, index):
-        return self._rows()[index]
-
-
-TRACKED_ETFS = _TrackedEtfs()
